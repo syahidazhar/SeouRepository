95ce11bfb138589b9b5731a632b7dd7c63313c60
syahidazhar <110156078+syahidazhar@users.noreply.github.com>
2022-07-28 14:20:50 +0700

    Create usermodel.js

---
+++ usermodel.js
@@ -0,0 +1,20 @@
+import {Sequelize} from "sequelize";
+import db from "../config/database.js";
+
+const {DataTypes} = Sequelize;
+
+const user = db.define('users',{
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    kelas: DataTypes.STRING,
+    alamat: DataTypes.STRING
+},{
+    freezeTableName:true
+});
+
+export default user;
+
+(async()=>{
+    await db.sync();
+})();
