44300fe82b5f723180e0ef638e22d1a338b047db
syahidazhar <110156078+syahidazhar@users.noreply.github.com>
2022-07-28 14:23:40 +0700

    Create addUser.js

---
+++ addUser.js
@@ -0,0 +1,98 @@
+import React,{useState} from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
+
+const AddUser = () => {
+const [name, setName] = useState("");
+const [email, setEmail] = useState("");
+const [gender, setGender] = useState("Male");
+const [kelas, setKelas] = useState("");
+const [alamat, setAlamat] = useState("");
+const navigate = useNavigate();
+
+const saveUser = async (e) =>{
+    e.preventDefault();
+    try {
+        await axios.post('http://localhost:5000/users',{
+            name,
+            email,
+            gender,
+            kelas,
+            alamat
+        })
+        navigate("/")
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+  return (
+    <div className="columns mt-5 is-centered">
+        <div className="column is-half">
+            <form onSubmit={saveUser}>
+                <div className="field">
+                    <label className="label">Name</label>
+                    <div className="control">
+                        <input type="text"
+                          className="input"
+                          value={name}
+                          onChange={(e)=> setName(e.target.value)}
+                          placeholder='Name'
+                        />
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label">Email</label>
+                    <div className="control">
+                        <input type="text"
+                          className="input"
+                          value={email}
+                          onChange={(e)=> setEmail(e.target.value)}
+                          placeholder='Email'
+                           />
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label">Gender</label>
+                    <div className="control">
+                        <div className="select is-fullwidth">
+                            <select value={gender}
+                            onChange={(e)=> setGender(e.target.value)}>
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label">Kelas</label>
+                    <div className="control">
+                        <input type="text"
+                          className="input"
+                          value={kelas}
+                          onChange={(e)=> setKelas(e.target.value)}
+                          placeholder='Kelas'
+                        />
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label">Alamat</label>
+                    <div className="control">
+                        <input type="text"
+                          className="input"
+                          value={alamat}
+                          onChange={(e)=> setAlamat(e.target.value)}
+                          placeholder='Alamat'
+                        />
+                    </div>
+                </div>
+                <div className="field">
+                    <button type='submit' className='button is-success'>Save</button>
+                </div>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default AddUser
